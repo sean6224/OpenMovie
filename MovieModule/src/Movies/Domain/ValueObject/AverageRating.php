2f90969b507a25d6e8a7ed07fecc610a3fb89915
nssean <kenny6224@protonmail.com>
2024-03-17 02:50:59 +0100

    Added __toString method to AverageRating Value Object

--- MovieModule/src/Movies/Domain/ValueObject/AverageRating.php
+++ MovieModule/src/Movies/Domain/ValueObject/AverageRating.php
@@ -5,5 +5,8 @@ namespace App\Movies\Domain\ValueObject;
 use App\Common\Domain\ValueObject\FloatValue;
 final class AverageRating extends FloatValue
 {
-
+    public function __toString(): string
+    {
+        return (string) $this->value;
+    }
 }
