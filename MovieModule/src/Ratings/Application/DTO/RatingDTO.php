46e13fc17c28de777b1435cdd7ce3767df90b7ed
nssean <kenny6224@protonmail.com>
2024-03-18 00:18:59 +0100

    Refactored RatingDTO with fromEntity method

--- MovieModule/src/Ratings/Application/DTO/RatingDTO.php
+++ MovieModule/src/Ratings/Application/DTO/RatingDTO.php
@@ -2,18 +2,20 @@
 declare(strict_types=1);
 namespace App\Ratings\Application\DTO;
 
+use App\Ratings\Domain\Entity\Rating;
+
 /**
- * Represents a Data Transfer Object (DTO) for information of rating.
+ * Represents a Data Transfer Object (DTO) for rating information.
  */
 final readonly class RatingDTO
 {
     /**
      * Constructs a new RatingDTO instance.
      *
-     * @param string $id The id of Rating.
-     * @param string $movieId The uuid movie of movie.
-     * @param string $userId The uuid user.
-     * @param float $averageRating User evaluation
+     * @param string $id The ID of rating.
+     * @param string $movieId The UUID of movie.
+     * @param string $userId The UUID of user.
+     * @param float $averageRating The average rating.
      */
     public function __construct(
         public string $id,
@@ -21,5 +23,21 @@ final readonly class RatingDTO
         public string $userId,
         public float $averageRating
     ) {
+    }
+
+    /**
+     * Create a RatingDTO from a Rating entity.
+     *
+     * @param Rating $rating The Rating entity to convert.
+     * @return RatingDTO The created RatingDTO.
+     */
+    public static function fromEntity(Rating $rating): self
+    {
+        return new self(
+            id: (string) $rating->id(),
+            movieId: (string) $rating->movieId(),
+            userId: (string) $rating->userId(),
+            averageRating: (float) $rating->averageRating(),
+        );
     }
 }
