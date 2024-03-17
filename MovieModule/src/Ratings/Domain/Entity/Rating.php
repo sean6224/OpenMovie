4c0b891dae54666a87836f7aab755b97c39b416b
nssean <kenny6224@protonmail.com>
2024-03-18 00:18:01 +0100

    Added Rating entity with constructor, factory method, and getter methods

---
+++ MovieModule/src/Ratings/Domain/Entity/Rating.php
@@ -0,0 +1,118 @@
+<?php
+declare(strict_types=1);
+namespace App\Ratings\Domain\Entity;
+
+use App\Common\Domain\Entity\AggregateRoot;
+use App\Common\Domain\ValueObject\DateTime;
+use App\Common\Domain\ValueObject\Id;
+
+use App\Ratings\Domain\ValueObject\MovieId;
+use App\Ratings\Domain\ValueObject\UserId;
+use App\Ratings\Domain\ValueObject\AverageRating;
+
+/**
+ * Represents Rating entity in the domain.
+ */
+class Rating extends AggregateRoot
+{
+    private Id $id;
+    private MovieId $movieId;
+    private UserId $userId;
+    private AverageRating $averageRating;
+    private DateTime $createdAt;
+
+    /**
+     * Constructs new Rating instance.
+     *
+     * @param DateTime $createdAt The date and time when the rating was created.
+     * @param MovieId $movieId The ID of the movie associated with the rating.
+     * @param UserId $userId The ID of the user who rated the movie.
+     * @param AverageRating $averageRating The average rating given to the movie.
+     */
+    private function __construct(
+        DateTime $createdAt,
+        MovieId $movieId,
+        UserId $userId,
+        AverageRating $averageRating
+    )
+    {
+        $this->id = Id::generate();
+        $this->movieId = $movieId;
+        $this->userId = $userId;
+        $this->averageRating = $averageRating;
+        $this->createdAt = $createdAt;
+    }
+
+    /**
+     * Factory method for creating new Rating instance.
+     *
+     * @param DateTime $createdAt The date and time when the rating was created.
+     * @param MovieId $movieId The ID of the movie associated with the rating.
+     * @param UserId $userId The ID of the user who rated the movie.
+     * @param AverageRating $averageRating The average rating given to the movie.
+     * @return Rating A new instance of the Rating entity.
+     */
+    public static function create(
+        DateTime $createdAt,
+        MovieId $movieId,
+        UserId $userId,
+        AverageRating $averageRating
+    ): self {
+        return new self(
+            createdAt: $createdAt,
+            movieId: $movieId,
+            userId: $userId,
+            averageRating: $averageRating
+        );
+    }
+
+    /**
+     * Gets the ID of rating.
+     *
+     * @return Id The ID of rating.
+     */
+    public function id(): Id
+    {
+        return $this->id;
+    }
+
+    /**
+     * Gets the ID of movie associated with rating.
+     *
+     * @return MovieId The ID of movie.
+     */
+    public function movieId(): MovieId
+    {
+        return $this->movieId;
+    }
+
+    /**
+     * Gets the ID of user who rated movie.
+     *
+     * @return UserId The ID of user.
+     */
+    public function userId(): UserId
+    {
+        return $this->userId;
+    }
+
+    /**
+     * Gets the average rating given to movie.
+     *
+     * @return AverageRating The average rating.
+     */
+    public function averageRating(): AverageRating
+    {
+        return $this->averageRating;
+    }
+
+    /**
+     * Gets the date and time when the rating was created.
+     *
+     * @return DateTime The creation date and time.
+     */
+    public function createdAt(): DateTime
+    {
+        return $this->createdAt;
+    }
+}
