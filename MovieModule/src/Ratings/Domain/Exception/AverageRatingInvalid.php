0bd468453438fc6e5e316fc270cd946d0e4901f1
nssean <kenny6224@protonmail.com>
2024-03-18 00:02:27 +0100

    Added AverageRatingInvalid exception for handling invalid average ratings.

---
+++ MovieModule/src/Ratings/Domain/Exception/AverageRatingInvalid.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+namespace App\Ratings\Domain\Exception;
+
+use DomainException;
+
+/**
+ * Exception thrown when attempting to create an entity with an invalid average rating.
+ */
+final class AverageRatingInvalid extends DomainException
+{
+    /**
+     * Constructs a new AverageRatingInvalid instance.
+     */
+    protected const INVALID_MESSAGE = 'Average rating must be between 0.0 and 10.0.';
+
+    public function __construct()
+    {
+        parent::__construct(self::INVALID_MESSAGE);
+    }
+}
+
