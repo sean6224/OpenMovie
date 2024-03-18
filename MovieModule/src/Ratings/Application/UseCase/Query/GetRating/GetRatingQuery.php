25b1456a08f4ff40da3bf09e6a202f7302feaa67
nssean <kenny6224@protonmail.com>
2024-03-18 14:25:02 +0100

    Add GetRatingQuery for retrieving information about a single movie rating

---
+++ MovieModule/src/Ratings/Application/UseCase/Query/GetRating/GetRatingQuery.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+namespace App\Ratings\Application\UseCase\Query\GetRating;
+
+use App\Common\Application\Query\Query;
+
+/**
+ * Query for retrieving information about single movie rating.
+ *
+ * This query is used to fetch details of specific movie rating based on its unique identifier (ID).
+ */
+final class GetRatingQuery implements Query
+{
+    /**
+     * Constructs a new GetRatingQuery instance.
+     *
+     * @param string $ratingId The unique identifier (ID) of movie rating.
+     */
+    public function __construct(
+        string $ratingId
+    ) {
+    }
+}
+
+
