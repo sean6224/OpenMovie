4c73dc2c4dfa1f4a85acd93c9c61406b29e4982a
nssean <kenny6224@protonmail.com>
2024-03-25 05:19:51 +0100

    Expanded functions for integration tests, added annotations, and optimized Faker invocation

--- MovieModule/tests/Movie/FakeDataMovie.php
+++ MovieModule/tests/Movie/FakeDataMovie.php
@@ -6,26 +6,49 @@ use App\Common\Application\Command\CommandBus;
 use App\Movies\Application\DTO\MovieBasicDTO;
 use App\Movies\Application\DTO\MovieDetailsParameterDTO;
 use App\Movies\Application\UseCase\Command\CreateMovie\CreateMovieCommand;
+use App\Movies\Domain\Entity\Movie;
 use App\Movies\Domain\Repository\MovieRepository;
+use App\Movies\Domain\ValueObject\AgeRestriction;
+use App\Movies\Domain\ValueObject\AverageRating;
+use App\Movies\Domain\ValueObject\Description;
+use App\Movies\Domain\ValueObject\Duration;
+use App\Movies\Domain\ValueObject\MovieName;
+use App\Movies\Domain\ValueObject\ReleaseYear;
 use Faker\Factory;
+use Faker\Generator;
+use InvalidArgumentException;
 
 class FakeDataMovie
 {
     private CommandBus $commandBus;
     private MovieRepository $movieRepository;
+    private Generator $faker;
+
     public function __construct(CommandBus $commandBus, MovieRepository $movieRepository)
     {
         $this->commandBus = $commandBus;
         $this->movieRepository = $movieRepository;
+        $this->faker = Factory::create();
     }
 
+    /**
+     * Creates movies using generated data.
+     *
+     * @param int $count The number of movies to create.
+     * @param bool $return Whether to return the ID of last created movie.
+     * @return string|null The ID of last created movie or null if no movies were created or $return is false.
+     */
     public function createMovie(int $count, bool $return = false): ?string
     {
-        $faker = Factory::create();
+        if ($count <= 0) {
+            throw new InvalidArgumentException('The count parameter must be greater than zero.');
+        }
 
+        $lastMovieId = null;
         for ($i = 0; $i < $count; $i++) {
-            $movieBasicData = $this->generateMovieBasicData($faker);
-            $movieDetailsParamData = $this->generateMovieDetailsParamData($faker);
+            $movieBasicData = $this->generateMovieBasicData();
+            $movieDetailsParamData = $this->generateMovieDetailsParamData();
+
             $createMovieCommand = new CreateMovieCommand(
                 movieName: $movieBasicData->movieName,
                 description: $movieBasicData->description,
@@ -36,14 +59,47 @@ class FakeDataMovie
                 averageRating: $movieBasicData->averageRating
             );
             $this->commandBus->dispatch($createMovieCommand);
-
-            if ($return) {
-                $movies = $this->movieRepository->findAll();
-                $movie = end($movies);
-                return $movie->id()->value();
-            }
         }
-        return null;
+
+        if ($return) {
+            $lastMovieId = $this->getLastMovieId();
+        }
+        return $return ? $lastMovieId : null;
+    }
+
+    /**
+     * Get ID of last movie in repository.
+     *
+     * @return string|null The ID of last movie or null if no movies are found.
+     */
+    private function getLastMovieId(): ?string
+    {
+        $movies = $this->movieRepository->findAll();
+        $lastMovie = end($movies);
+        return $lastMovie ? $lastMovie->id()->value() : null;
+    }
+
+    /**
+     * Generates movie using generated basic and detailed data.
+     *
+     * This method is intended for use in integration tests only.
+     *
+     * @return Movie The generated movie.
+     */
+    private function generateMovie(): Movie
+    {
+        $movieBasicData = $this->generateMovieBasicData();
+        $movieDetailsParamData = $this->generateMovieDetailsParamData();
+
+        return Movie::create(
+            MovieName::fromString($movieBasicData->movieName),
+            Description::fromString($movieBasicData->description),
+            ReleaseYear::fromString($movieBasicData->releaseYear),
+            $movieDetailsParamData->toArray(),
+            Duration::fromInt($movieBasicData->duration),
+            AgeRestriction::fromInt($movieBasicData->ageRestriction),
+            AverageRating::fromFloat($movieBasicData->averageRating)
+        );
     }
 
     /**
@@ -51,15 +107,15 @@ class FakeDataMovie
      *
      * @return MovieBasicDTO The basic movie data.
      */
-    public function generateMovieBasicData($faker): MovieBasicDTO
+    private function generateMovieBasicData(): MovieBasicDTO
     {
         return new MovieBasicDTO(
-            movieName: $faker->name(),
-            description: $faker->sentence(),
-            releaseYear: $faker->year(),
-            duration: $faker->numberBetween(0, 90),
-            ageRestriction: $faker->numberBetween(0, 1500),
-            averageRating: $faker->randomFloat(1, 1, 10)
+            movieName: $this->faker->name(),
+            description: $this->faker->sentence(),
+            releaseYear: $this->faker->year(),
+            duration: $this->faker->numberBetween(0, 90),
+            ageRestriction: $this->faker->numberBetween(0, 1500),
+            averageRating: $this->faker->randomFloat(1, 1, 10)
         );
     }
 
@@ -68,16 +124,16 @@ class FakeDataMovie
      *
      * @return MovieDetailsParameterDTO The detailed movie data.
      */
-    public function generateMovieDetailsParamData($faker): MovieDetailsParameterDTO
+    private function generateMovieDetailsParamData(): MovieDetailsParameterDTO
     {
         return new MovieDetailsParameterDTO(
-            productionCountry: [$faker->country()],
-            directors: [$faker->name()],
-            actors: [$faker->name()],
-            category: [$faker->word()],
-            tags: [$faker->word()],
-            languages: [$faker->locale()],
-            subtitles: [$faker->word()]
+            productionCountry: [$this->faker->country()],
+            directors: [$this->faker->name()],
+            actors: [$this->faker->name()],
+            category: [$this->faker->word()],
+            tags: [$this->faker->word()],
+            languages: [$this->faker->locale()],
+            subtitles: [$this->faker->word()]
         );
     }
 }
