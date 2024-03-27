b3bc82a90e661f3f2ee67aaf5d1c907dc70b170a
nssean <kenny6224@protonmail.com>
2024-03-27 23:00:54 +0100

    Added Command Delete rating

---
+++ MovieModule/src/Ratings/Application/UseCase/Command/DeleteRating/DeleteRatingCommandHandler.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+namespace App\Ratings\Application\UseCase\Command\DeleteRating;
+
+use App\Common\Application\Command\CommandHandler;
+use App\Common\Domain\ValueObject\Id;
+use App\Ratings\Application\Service\DeleteRating;
+
+class DeleteRatingCommandHandler  implements CommandHandler
+{
+    /**
+     * Constructs new DeleteRatingCommandHandler instance.
+     *
+     * @param DeleteRating $deleteRating The service responsible for deleting rating.
+     */
+    public function __construct(
+        private DeleteRating $deleteRating
+    ) {
+    }
+
+
+    /**
+     * Handles the DeleteMovieCommand.
+     *
+     * @param DeleteRatingCommand $command The command to delete rating.
+     * @return void
+     */
+    public function __invoke(DeleteRatingCommand $command): void
+    {
+        $this->deleteRating->deleteMovie(
+            Id::fromString($command->ratingId)
+        );
+    }
+}
