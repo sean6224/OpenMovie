0b7ad7679eabf1361cdb29d3700b36be3c98abbe
nssean <kenny6224@protonmail.com>
2024-03-30 16:47:27 +0100

    Update SearchMoviesByCriteriaProcessor to return MovieSearchOutput

--- Backend/MovieModule/src/Movies/UserInterface/ApiPlatform/Processor/Search/SearchMoviesByCriteriaProcessor.php
+++ Backend/MovieModule/src/Movies/UserInterface/ApiPlatform/Processor/Search/SearchMoviesByCriteriaProcessor.php
@@ -4,10 +4,12 @@ namespace App\Movies\UserInterface\ApiPlatform\Processor\Search;
 
 use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProcessorInterface;
-use App\Common\Application\Command\CommandBus;
+use App\Common\Application\Query\QueryBus;
+use App\Movies\Application\DTO\MovieDTO;
 use App\Movies\Domain\Exception\MissingOrEmptyAttributesException;
-use App\Movies\Application\UseCase\Query\Search\SearchMoviesByCriteria\SearchMoviesByCriteriaCommand;
-use App\Movies\UserInterface\ApiPlatform\Resource\SearchResource;
+use App\Movies\Application\UseCase\Query\Search\SearchMoviesByCriteria\SearchMoviesByCriteriaQuery;
+use App\Movies\UserInterface\ApiPlatform\Output\MovieSearchOutput;
+use App\Movies\UserInterface\ApiPlatform\Resource\MovieResource;
 
 /**
  * The SearchMoviesByCriteriaProcessor is responsible for processing input data
@@ -20,28 +22,28 @@ final readonly class SearchMoviesByCriteriaProcessor implements ProcessorInterfa
     /**
      * Creates new instance of SearchMoviesByCriteriaProcessor.
      *
-     * @param CommandBus $commandBus The CommandBus object to handle search command.
+     * @param QueryBus $queryBus
      */
     public function __construct(
-        private CommandBus $commandBus,
+        private QueryBus   $queryBus,
     ) {
     }
 
     /**
      * Processes the input data to perform movie search operation.
      *
-     * @param mixed     $data          Input data for movie search operation.
-     * @param Operation $operation     Metadata about the processing operation.
-     * @param array     $uriVariables  Variables extracted from URI.
-     * @param array     $context       Additional context for processing.
-     * @return SearchResource          The resource representing search results.
+     * @param mixed $data Input data for movie search operation.
+     * @param Operation $operation Metadata about the processing operation.
+     * @param array $uriVariables Variables extracted from URI.
+     * @param array $context Additional context for processing.
+     * @return MovieSearchOutput The array representing search results.
      */
-    public function process(mixed $data, Operation $operation, array $uriVariables = [], array $context = []): SearchResource
+    public function process(mixed $data, Operation $operation, array $uriVariables = [], array $context = []): MovieSearchOutput
     {
         $this->validate($data);
 
-        return $this->commandBus->dispatch(
-            new SearchMoviesByCriteriaCommand(
+        $movies = $this->queryBus->ask(
+            new SearchMoviesByCriteriaQuery(
                 filters: $data->getFilters(),
                 sort: $data->getSort(),
                 order: $data->getOrder(),
@@ -49,6 +51,8 @@ final readonly class SearchMoviesByCriteriaProcessor implements ProcessorInterfa
                 pageSize: $data->getPageSize()
             )
         );
+        $movieResources = $this->mapMovieDTOsToMovieResources($movies);
+        return new MovieSearchOutput($movieResources);
     }
 
     /**
@@ -75,5 +79,21 @@ final readonly class SearchMoviesByCriteriaProcessor implements ProcessorInterfa
         if (!empty($missingAttributes)) {
             throw new MissingOrEmptyAttributesException($missingAttributes);
         }
+    }
+
+
+    /**
+     * Maps movie DTOs to movie resources.
+     *
+     * @param MovieDTO[] $movieDTOs The movie DTOs to map
+     * @return MovieResource[] The mapped movie resources
+     */
+    private function mapMovieDTOsToMovieResources(array $movieDTOs): array
+    {
+        $searchResources = [];
+        foreach ($movieDTOs as $movieDTO) {
+            $searchResources[] = MovieResource::fromMovieDTO($movieDTO);
+        }
+        return $searchResources;
     }
 }
