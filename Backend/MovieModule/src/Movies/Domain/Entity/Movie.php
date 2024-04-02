e860a6917ae6092ce65f3ce17bdcd42e4eb4d043
nssean <kenny6224@protonmail.com>
2024-04-02 02:58:04 +0200

    Refactor Movie Entity with ProductionCountry Value Object

--- Backend/MovieModule/src/Movies/Domain/Entity/Movie.php
+++ Backend/MovieModule/src/Movies/Domain/Entity/Movie.php
@@ -5,16 +5,21 @@ namespace App\Movies\Domain\Entity;
 use App\Common\Domain\Entity\AggregateRoot;
 use App\Common\Domain\ValueObject\DateTime;
 use App\Common\Domain\ValueObject\Id;
-
-use App\Movies\Domain\ValueObject\MovieName;
-use App\Movies\Domain\ValueObject\Description;
-use App\Movies\Domain\ValueObject\ReleaseYear;
-use App\Movies\Domain\ValueObject\Duration;
 use App\Movies\Domain\ValueObject\AgeRestriction;
 use App\Movies\Domain\ValueObject\AverageRating;
-
+use App\Movies\Domain\ValueObject\Description;
+use App\Movies\Domain\ValueObject\Duration;
+use App\Movies\Domain\ValueObject\MovieName;
+use App\Movies\Domain\ValueObject\ProductionCountry;
+use App\Movies\Domain\ValueObject\ReleaseYear;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+
+/**
+ * Class Movie
+ *
+ * Represents movie entity with basic information and associated details.
+ */
 class Movie extends AggregateRoot
 {
     private Id $id;
@@ -24,283 +29,85 @@ class Movie extends AggregateRoot
     private Duration $duration;
     private AgeRestriction $ageRestriction;
     private AverageRating $averageRating;
+    private ProductionCountry $productionCountry;
 
-    /** @var Collection<int, ProductionCountry> */
-    private Collection $productionCountries;
-    /** @var Collection<int, Directors> */
-    private Collection $directors;
-    /** @var Collection<int, Actors> */
-    private Collection $actors;
-    /** @var Collection<int, Category> */
-    private Collection $category;
-    /** @var Collection<int, Language> */
-    private Collection $language;
-    /** @var Collection<int, Subtitles> */
-    private Collection $subtitles;
-
+    /** @var Collection<int, ProductionLocationManager> */
+    private Collection $productionLocationsManager;
+    /** @var Collection<int, DirectorsManager> */
+    private Collection $directorsManager;
+    /** @var Collection<int, ActorsManager> */
+    private Collection $actorsManager;
+    /** @var Collection<int, CategoryManager> */
+    private Collection $categoryManager;
+    /** @var Collection<int, LanguageManager> */
+    private Collection $languageManager;
+    /** @var Collection<int, SubtitlesManager> */
+    private Collection $subtitlesManager;
     private DateTime $createdAt;
+
     private function __construct(
-        MovieName $movieName,
-        Description $description,
-        ReleaseYear $releaseYear,
+        array $movieBasic,
         array $movieDetails,
-        Duration $duration,
-        AgeRestriction $ageRestriction,
-        AverageRating $averageRating
     ) {
-
         $this->id = Id::generate();
+        $this->initialize($movieBasic);
         $this->createdAt = DateTime::now();
-
-        $this->movieName = $movieName;
-        $this->description = $description;
-        $this->releaseYear = $releaseYear;
-
-        $this->initializeCollections($movieDetails);
-        $this->duration = $duration;
-        $this->ageRestriction = $ageRestriction;
-        $this->averageRating = $averageRating;
+        $this->initializeCollection($movieDetails);
     }
 
-    /**
-     * Create a new instance of the class.
-     *
-     * @param MovieName $movieName The name of the movie.
-     * @param Description $description The description of the movie.
-     * @param ReleaseYear $releaseYear The release year of the movie.
-     * @param Duration $duration The duration of the movie.
-     * @param AgeRestriction $ageRestriction The age restriction of the movie.
-     * @return self A new instance of the class with the specified parameters.
-     */
     public static function create(
-        MovieName $movieName,
-        Description $description,
-        ReleaseYear $releaseYear,
+        array $movieBasic,
         array $movieDetails,
-        Duration $duration,
-        AgeRestriction $ageRestriction,
-        AverageRating $averageRating,
     ): self {
         return new self(
-            movieName: $movieName,
-            description: $description,
-            releaseYear: $releaseYear,
+            movieBasic: $movieBasic,
             movieDetails: $movieDetails,
-            duration: $duration,
-            ageRestriction: $ageRestriction,
-            averageRating: $averageRating
         );
     }
 
-    /**
-     * Updates the movie entity with the provided data.
-     *
-     * @param MovieName|null $movieName The new movie name, or null to keep the existing one.
-     * @param Description|null $description The new movie description, or null to keep the existing one.
-     * @param ReleaseYear|null $releaseYear The new release year, or null to keep the existing one.
-     * @param array|null $movieDetails The new movie details, or null to keep the existing ones.
-     * @param Duration|null $duration The new movie duration, or null to keep the existing one.
-     * @param AgeRestriction|null $ageRestriction The new age restriction, or null to keep the existing one.
-     * @return void
-     */
-    public function update(
-        ?MovieName $movieName = null,
-        ?Description $description = null,
-        ?ReleaseYear $releaseYear = null,
-        ?array $movieDetails = null,
-        ?Duration $duration = null,
-        ?AgeRestriction $ageRestriction = null
-    ): void {
-        $this->movieName = $movieName ?? $this->movieName;
-        $this->description = $description ?? $this->description;
-        $this->releaseYear = $releaseYear ?? $this->releaseYear;
-        $this->duration = $duration ?? $this->duration;
-        $this->ageRestriction = $ageRestriction ?? $this->ageRestriction;
-
-        if ($movieDetails !== null) {
-            $this->initializeCollections($movieDetails);
-        }
+    private function initialize(array $movieBasic): void
+    {
+        $this->movieName = MovieName::fromString($movieBasic['movieName']);
+        $this->description = Description::fromString($movieBasic['description']);
+        $this->releaseYear = ReleaseYear::fromString($movieBasic['releaseYear']);
+        $this->duration = Duration::fromInt($movieBasic['duration']);
+        $this->ageRestriction = AgeRestriction::fromInt($movieBasic['ageRestriction']);
+        $this->averageRating = AverageRating::fromFloat($movieBasic['averageRating']);
+        $this->productionCountry = ProductionCountry::fromString($movieBasic['productionCountry']);
     }
 
-    /**
-     * Initialize collections for movie details.
-     *
-     * This method initializes various collections for movie details such as production country, directors, actors,
-     * category, languages, and subtitles.
-     *
-     * @param array $movieDetails The array containing movie details.
-     * @return void
-     */
-    private function initializeCollections(array $movieDetails): void
+    private function initializeCollection(array $movieDetails): void
     {
-        $this->productionCountries = new ArrayCollection();
-        $this->directors = new ArrayCollection();
-        $this->actors = new ArrayCollection();
-        $this->category = new ArrayCollection();
-        $this->language = new ArrayCollection();
-        $this->subtitles = new ArrayCollection();
+        $this->productionLocationsManager = new ArrayCollection();
+        $this->directorsManager = new ArrayCollection();
+        $this->actorsManager = new ArrayCollection();
+        $this->categoryManager = new ArrayCollection();
+        $this->languageManager = new ArrayCollection();
+        $this->subtitlesManager = new ArrayCollection();
 
         $collectionMappings = [
-            'productionCountry' => ProductionCountry::class,
-            'directors' => Directors::class,
-            'actors' => Actors::class,
-            'category' => Category::class,
-            'languages' => Language::class,
-            'subtitles' => Subtitles::class,
+            'productionLocations' => ProductionLocationManager::class,
+            'directors' => DirectorsManager::class,
+            'actors' => ActorsManager::class,
+            'category' => CategoryManager::class,
+            'languages' => LanguageManager::class,
+            'subtitles' => SubtitlesManager::class,
         ];
 
-        foreach ($collectionMappings as $collectionName => $class)
-        {
-            $this->initializeCollectionFor($collectionName, $class, $movieDetails);
+        foreach ($collectionMappings as $detailKey => $managerClass) {
+            $this->{$detailKey . 'Manager'} = $this->createCollection($movieDetails[$detailKey] ?? [], $managerClass);
         }
     }
 
-    /**
-     * Initialize a specific collection for movie details.
-     *
-     * This method initializes a specific collection for movie details based on the provided collection name and class.
-     *
-     * @param string $collectionName The name of collection to initialize.
-     * @param string $class The class name of collection.
-     * @param array $movieDetails The array containing movie details.
-     * @return void
-     */
-    private function initializeCollectionFor(string $collectionName, string $class, array $movieDetails): void
+    private function createCollection(array $items, string $managerClass): Collection
     {
-        $addMethod = 'add' . ucfirst($collectionName);
-        $objects = array_map(
-            fn($item) => $class::create($item, $this),
-            $movieDetails[$collectionName]
-        );
-        array_map([$this, $addMethod], $objects);
+        $collection = new ArrayCollection();
+        foreach ($items as $item) {
+            $manager = call_user_func([$managerClass, 'create'], $item, $this);
+            $collection->add($manager);
+        }
+        return $collection;
     }
-
-    /**
-     * Add a production country to the movie.
-     *
-     * @param ProductionCountry $productionCountry The production country to add.
-     * @return void
-     */
-    public function addProductionCountry(ProductionCountry $productionCountry): void
-    {
-        $this->productionCountries->add($productionCountry);
-    }
-
-    /**
-     * Add directors to the movie.
-     *
-     * @param Directors $directors The directors to add.
-     * @return void
-     */
-    public function addDirectors(Directors $directors): void
-    {
-        $this->directors->add($directors);
-    }
-
-    /**
-     * Add actors to the movie.
-     *
-     * @param Actors $actors The actors to add.
-     * @return void
-     */
-    public function addActors(Actors $actors): void
-    {
-        $this->actors->add($actors);
-    }
-
-    /**
-     * Add a category to the movie.
-     *
-     * @param Category $category The category to add.
-     * @return void
-     */
-    public function addCategory(Category $category): void
-    {
-        $this->category->add($category);
-    }
-
-    /**
-     * Add languages to the movie.
-     *
-     * @param Language $language The languages to add.
-     * @return void
-     */
-    public function addLanguages(Language $language): void
-    {
-        $this->language->add($language);
-    }
-
-    /**
-     * Add subtitles to the movie.
-     *
-     * @param Subtitles $subtitles The subtitles to add.
-     * @return void
-     */
-    public function addSubtitles(Subtitles $subtitles): void
-    {
-        $this->subtitles->add($subtitles);
-    }
-
-    /**
-     * Get the production countries associated with the movie.
-     *
-     * @return array The production countries associated with the movie.
-     */
-    public function productionCountry(): array
-    {
-        return $this->productionCountries->toArray();
-    }
-
-    /**
-     * Get the directors of the movie.
-     *
-     * @return array The directors of the movie.
-     */
-    public function Directors(): array
-    {
-        return $this->directors->toArray();
-    }
-
-    /**
-     * Get the actors of the movie.
-     *
-     * @return array The actors of the movie.
-     */
-    public function Actors(): array
-    {
-        return $this->actors->toArray();
-    }
-
-    /**
-     * Get the category of the movie.
-     *
-     * @return array The category of the movie.
-     */
-    public function Category(): array
-    {
-        return $this->category->toArray();
-    }
-
-    /**
-     * Get the languages associated with the movie.
-     *
-     * @return array The languages associated with the movie.
-     */
-    public function Languages(): array
-    {
-        return $this->language->toArray();
-    }
-
-    /**
-     * Get the subtitles associated with the movie.
-     *
-     * @return array The subtitles associated with the movie.
-     */
-    public function Subtitles(): array
-    {
-        return $this->subtitles->toArray();
-    }
-
 
     /**
      * Retrieves the ID of object
@@ -313,7 +120,7 @@ class Movie extends AggregateRoot
     }
 
     /**
-     * Returns name of the movie.
+     * Returns name of movie.
      *
      * @return MovieName The name of movie.
      */
@@ -323,9 +130,9 @@ class Movie extends AggregateRoot
     }
 
     /**
-     * Retrieves the description of object.
+     * Retrieves description of object.
      *
-     * @return Description The description of object.
+     * @return Description description of object.
      */
     public function getDescription(): Description
     {
@@ -333,9 +140,9 @@ class Movie extends AggregateRoot
     }
 
     /**
-     * Get the release year of movie.
+     * Get release year of movie.
      *
-     * @return ReleaseYear The release year of movie.
+     * @return ReleaseYear release year of movie.
      */
     public function getReleaseYear(): ReleaseYear
     {
@@ -343,9 +150,9 @@ class Movie extends AggregateRoot
     }
 
     /**
-     * Returns the duration of a movie.
+     * Returns duration of movie.
      *
-     * @return Duration The duration of movie.
+     * @return Duration duration of movie.
      */
     public function getDuration(): Duration
     {
@@ -353,9 +160,9 @@ class Movie extends AggregateRoot
     }
 
     /**
-     * Retrieve the age restriction for a movie.
+     * Retrieve age restriction for movie.
      *
-     * @return AgeRestriction The age restriction of movie.
+     * @return AgeRestriction age restriction of movie.
      */
     public function getAgeRestriction(): AgeRestriction
     {
@@ -363,9 +170,9 @@ class Movie extends AggregateRoot
     }
 
     /**
-     * Retrieve the average rating for a movie.
+     * Retrieve average rating for movie.
      *
-     * @return AverageRating The average rating of movie.
+     * @return AverageRating average rating of movie.
      */
     public function getAverageRating(): AverageRating
     {
@@ -373,12 +180,82 @@ class Movie extends AggregateRoot
     }
 
     /**
-     * Retrieve the createdAt for a movie.
+     * Retrieve Production country for movie.
      *
-     * @return DateTime The createdAt of movie.
+     * @return ProductionCountry Production country of movie.
+     */
+    public function getProductionCountry(): ProductionCountry
+    {
+        return $this->productionCountry;
+    }
+
+    /**
+     * Retrieve createdAt for movie.
+     *
+     * @return DateTime createdAt of movie.
      */
     public function getDateTime(): DateTime
     {
         return $this->createdAt;
+    }
+
+    /**
+     * Get the production locations associated with movie.
+     *
+     * @return array The production locations associated with movie.
+     */
+    public function getProductionLocationsManager(): array
+    {
+        return $this->productionLocationsManager->toArray();
+    }
+
+    /**
+     * Get the directors of movie.
+     *
+     * @return array The directors of movie.
+     */
+    public function getDirectorsManager(): array
+    {
+        return $this->directorsManager->toArray();
+    }
+
+    /**
+     * Get the actors of movie.
+     *
+     * @return array The actors of movie.
+     */
+    public function getActorsManager(): array
+    {
+        return $this->actorsManager->toArray();
+    }
+
+    /**
+     * Get the category of movie.
+     *
+     * @return array The category of movie.
+     */
+    public function getCategoryManager(): array
+    {
+        return $this->categoryManager->toArray();
+    }
+
+    /**
+     * Get the languages associated with movie.
+     *
+     * @return array The languages associated with movie.
+     */
+    public function getLanguagesManager(): array
+    {
+        return $this->languageManager->toArray();
+    }
+
+    /**
+     * Get the subtitles associated with movie.
+     *
+     * @return array The subtitles associated with movie.
+     */
+    public function getSubtitlesManager(): array
+    {
+        return $this->subtitlesManager->toArray();
     }
 }
