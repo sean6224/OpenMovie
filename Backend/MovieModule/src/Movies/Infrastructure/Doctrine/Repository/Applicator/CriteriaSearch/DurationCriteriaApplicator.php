a7a2b31055f1c02c37b42b55dc610b7e353ee193
nssean <kenny6224@protonmail.com>
2024-04-03 00:39:00 +0200

    Implemented DurationCriteriaApplicator for Applying Duration Criteria to Query Builder

---
+++ Backend/MovieModule/src/Movies/Infrastructure/Doctrine/Repository/Applicator/CriteriaSearch/DurationCriteriaApplicator.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types=1);
+namespace App\Movies\Infrastructure\Doctrine\Repository\Applicator\CriteriaSearch;
+
+use Doctrine\ORM\QueryBuilder;
+
+/**
+ * Class DurationCriteriaApplicator
+ *
+ * Implements the CriteriaApplicatorInterface to apply duration criteria to query builder.
+ */
+class DurationCriteriaApplicator implements CriteriaApplicatorInterface
+{
+    /**
+     * Applies duration criteria to provided query builder.
+     *
+     * @param QueryBuilder $queryBuilder The query builder instance.
+     * @param array $criteria The criteria array containing duration filters.
+     * @return void
+     */
+    public function apply(QueryBuilder $queryBuilder, array $criteria): void
+    {
+        if (isset($criteria['min-duration']) && $criteria['min-duration'] > 0) {
+            $queryBuilder
+                ->andWhere('m.duration >= :minDuration')
+                ->setParameter('minDuration', $criteria['min-duration']);
+        }
+
+        if (isset($criteria['max-duration'])) {
+            $queryBuilder
+                ->andWhere('m.duration <= :maxDuration')
+                ->setParameter('maxDuration', $criteria['max-duration']);
+        }
+    }
+}
