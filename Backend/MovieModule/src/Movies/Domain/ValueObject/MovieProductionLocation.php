4c5bfdcdf339e64b77fa0780c5339f0840d109bd
nssean <kenny6224@protonmail.com>
2024-04-02 02:21:54 +0200

    Added new ValueObject MovieProductionLocation

---
+++ Backend/MovieModule/src/Movies/Domain/ValueObject/MovieProductionLocation.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+namespace App\Movies\Domain\ValueObject;
+
+use App\Common\Domain\ValueObject\StringValue;
+final class MovieProductionLocation extends StringValue
+{
+
+}
