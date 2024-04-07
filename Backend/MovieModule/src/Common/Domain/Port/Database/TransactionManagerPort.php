ce44485528a94edc6484c5142bb4a7304845eaca
nssean <kenny6224@protonmail.com>
2024-04-07 04:40:18 +0200

    Updated TransactionManagerPort interface to include savepoint management

--- Backend/MovieModule/src/Common/Domain/Port/Database/TransactionManagerPort.php
+++ Backend/MovieModule/src/Common/Domain/Port/Database/TransactionManagerPort.php
@@ -3,14 +3,35 @@ declare(strict_types=1);
 namespace App\Common\Domain\Port\Database;
 
 /**
-* Interface representing a port for managing transactions in the database.
-*/
+ * Interface representing port for managing transactions and savepoints in database.
+ */
 interface TransactionManagerPort
 {
     /**
      * Starts transaction in database.
      */
     public function beginTransaction(): void;
+
+    /**
+     * Creates savepoint within current transaction.
+     *
+     * @param string $name Savepoint name.
+     */
+    public function createSavepoint(string $name): void;
+
+    /**
+     * Releases previously created savepoint.
+     *
+     * @param string $name Savepoint name.
+     */
+    public function releaseSavepoint(string $name): void;
+
+    /**
+     * Rolls back to previously created savepoint.
+     *
+     * @param string $name Savepoint name.
+     */
+    public function rollBackToSavepoint(string $name): void;
 
     /**
      * Approves transaction.
